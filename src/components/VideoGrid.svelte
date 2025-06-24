adc28d503acebc08844dd1caea258ceeede74acd
doxy <doxy.desu@gmail.com>
2025-06-23 18:35:37 -0700

    add placeholders for loading video types

--- src/components/VideoGrid.svelte
+++ src/components/VideoGrid.svelte
@@ -1,22 +1,38 @@
 <script lang="ts">
-    import { buildApiUrl, buildImageUrl, fetchApiVideoResponse, type VideoType } from '~/utils/archive';
-    import VideoGridItem from './VideoGridItem.svelte';
-    let { videoPath, videoType }: { videoPath: string, videoType: VideoType } = $props();
+  import {
+    buildApiUrl,
+    buildImageUrl,
+    fetchApiVideoResponse,
+    type VideoType,
+  } from "~/utils/archive";
+  import VideoGridItem from "./VideoGridItem.svelte";
+  let { videoPath, videoType }: { videoPath: string; videoType: VideoType } =
+    $props();
 
-    const path = buildApiUrl(videoPath, videoType)
-    const request = fetchApiVideoResponse(path)
+  const path = buildApiUrl(videoPath, videoType);
+  const request = fetchApiVideoResponse(path);
 </script>
 
-{#await request}
-<div>Put a spinner here or some bs...</div>
-
-{:then videos}
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-    {#each videos as video}
+<div class="p-2">
+  {#await request}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+      {#each [0, 1, 2, 3, 4, 5] as {}}
+        <div class="w-full animate-pulse">
+          <div class="bg-gray-700 aspect-video rounded w-full flex align-items-center justify-center">
+            <div class="m-auto size-10 iconify line-md--loading-twotone-loop"></div>
+          </div>
+          <div class="bg-gray-700 h-4 w-2/3 rounded-2xl mt-4"></div>
+          <div class="bg-gray-700 h-2 w-1/3 rounded mt-4"></div>
+        </div>
+      {/each}
+    </div>
+  {:then videos}
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 p-2">
+      {#each videos as video}
         <VideoGridItem {video} {videoPath} {videoType}></VideoGridItem>
-    {/each}
+      {/each}
+    </div>
+  {:catch error}
+    <p>Something went horribly wrong... {error}</p>
+  {/await}
 </div>
-
-{:catch error}
-<p>Something went horribly wrong... {error}</p>
-{/await}
--- src/components/VideoGridItem.svelte
+++ src/components/VideoGridItem.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { buildImageUrl, buildVideoUrl, buildWatchLink, type VideoDetails, type VideoType } from '~/utils/archive'
+    import { buildImageUrl, buildWatchLink, type VideoDetails, type VideoType } from '~/utils/archive'
     import { getRelativeTime } from '~/utils/std';
     let { videoPath, videoType, video }: { videoPath: string, videoType: VideoType, video: VideoDetails } = $props();
 
@@ -7,9 +7,9 @@
     let watchLink = buildWatchLink(videoType, video.id)
 </script>
 
-<a class="relative" href={watchLink}>
+<a class="relative group" href={watchLink}>
     <img class="aspect-video" src={imageUrl} alt="video thumbnail" />
-    <div class="font-bold">{video.title}</div>
+    <div class="font-bold group-hover:underline pt-1">{video.title}</div>
     <div class="font-light text-sm">{getRelativeTime(video.createdAt)}</div>
-    <div class="absolute top-0.5 left-0.5 bg-gray-900 font-mono opacity-70">{video.duration}</div>
+    <div class="absolute top-0.5 left-0.5 bg-gray-900 font-mono opacity-90">{video.duration}</div>
 </a>
