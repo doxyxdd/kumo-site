703fb6d01bc06fccec3f698b23deabcccceb134f
doxy <doxy.desu@gmail.com>
2025-06-23 18:38:21 -0700

    minor ui changes to grid item

--- src/components/VideoGridItem.svelte
+++ src/components/VideoGridItem.svelte
@@ -10,6 +10,6 @@
 <a class="relative group" href={watchLink}>
     <img class="aspect-video" src={imageUrl} alt="video thumbnail" />
     <div class="font-bold group-hover:underline pt-1">{video.title}</div>
-    <div class="font-light text-sm">{getRelativeTime(video.createdAt)}</div>
-    <div class="absolute top-0.5 left-0.5 bg-gray-900 font-mono opacity-90">{video.duration}</div>
+    <div class="font-light text-xs text-gray-300 font-mono italic">{getRelativeTime(video.createdAt)}</div>
+    <div class="absolute top-0.5 left-0.5 bg-black font-mono opacity-80">{video.duration}</div>
 </a>
