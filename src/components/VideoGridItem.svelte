02d89e67a91cbb57b53ab0b5b56d8192cea3fdec
doxy <doxy.desu@gmail.com>
2025-06-23 18:52:37 -0700

    fix img reflow

--- src/components/VideoGridItem.svelte
+++ src/components/VideoGridItem.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <a class="relative group" href={watchLink}>
-    <img class="aspect-video" src={imageUrl} alt="video thumbnail" />
+    <img class="aspect-video w-full" src={imageUrl} alt="video thumbnail" />
     <div class="font-bold group-hover:underline pt-1">{video.title}</div>
     <div class="font-light text-xs text-gray-300 font-mono italic">{getRelativeTime(video.createdAt)}</div>
     <div class="absolute top-0.5 left-0.5 bg-black font-mono opacity-80">{video.duration}</div>
